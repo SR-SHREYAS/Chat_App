f7f624b7df0e51d1e1a349ce93ff27263b245cb4
SR-SHREYAS <shreyanshu2585.shreyas@gmail.com>
2025-10-20 20:12:21 +0530

    real time chat app version 1.3

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module real_time_chat_app
 go 1.25.2
 
 require github.com/gorilla/websocket v1.5.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 type templateHandler struct {
@@ -28,11 +30,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	// Load .env file, but don't fail if it's not present (for deployment)
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using environment variables from system")
+	}
+
 	// make every randomly generated number unique
 	rand.Seed(time.Now().UnixNano())
 
-	var addr = flag.String("addr", ":8080", "The addr of the application")
-	flag.Parse()
+	// var addr = flag.String("addr", ":8080", "The addr of the application")
+	// flag.Parse()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := ":" + port
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/", &templateHandler{filename: "index.html"})
@@ -47,16 +60,41 @@ func main() {
 		realRoom := getRoom(roomName) // Get the room instance
 		realRoom.ServeHTTP(w, r)      // Call the ServeHTTP method on the room instance
 	})
-	// This will be the websocket endpoint
 
-	// run the room in a separate goroutine
+	// Health check endpoint
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	//start the web server
 
-	log.Println("starting web server on", *addr)
+	log.Println("starting web server on", addr)
 
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	if err := http.ListenAndServe(addr, CORSMiddleware(http.DefaultServeMux)); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 
+}
+
+// CORSMiddleware adds the necessary headers to handle Cross-Origin Resource Sharing.
+// This is useful if you ever decide to host your frontend on a different domain.
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set headers to allow cross-origin requests
+		// Note: Using "*" for Access-Control-Allow-Origin is permissive.
+		// For production, you should restrict this to your frontend's domain.
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// If this is a preflight request (OPTIONS), we can just send an OK status.
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Otherwise, serve the request to the next handler.
+		next.ServeHTTP(w, r)
+	})
 }
